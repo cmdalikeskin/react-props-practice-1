9f29cb52cec44bf73b6f63a96c1f0148f03c84bb
Ali Keskin <cmdalikeskin@gmail.com>
2023-08-18 10:04:39 +0200

    Ali Keskin - props practice exercise

---
+++ src/NameLength.js
@@ -0,0 +1,8 @@
+function NameLength(props) {
+    return (
+        <p className="name-length">
+            Did you know that {props.name} is {props.name.length} characters long?!
+        </p>
+    )
+}
+export default NameLength
